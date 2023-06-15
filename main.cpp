450ed9201f92a282376cce38fb35912ecf2432ab
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-06-15 11:40:37 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+#include <ctime>
+
+
+
+int main()
+{   
+    setlocale(0, "RUS");
+    int input;
+
+    cout << "Выберите что нарисовать:\n";
+    cout << "1:Главную диагональ с пространством сверху.\n2:Главную диагональ с пространством снизу.\n3:Триугольник сверху.\n4:Триугольник слева.\n";
+    cout << "5:Побочная диагональ с пространством сверху.\n6:Побочная диагональ с пространством снизу.\n";
+    cin >> input;
+    switch(input) 
+    {
+    case 1:
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i == j || i < j)
+                    cout << "#";
+                else
+                    cout << " ";
+            }
+            cout << "\n";
+        }
+    break;
+    case 2: 
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i == j || i > j)
+                    cout << "#";
+                else
+                    cout << " ";
+            }
+            cout << "\n";
+        }
+    break;
+    case 3:
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10 - i; j++)
+            {
+                if (i < j)
+                    cout << "#";
+                else
+                    cout << " ";
+            }
+            cout << "\n";
+        }
+    break;
+    case 4:
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10 - i; j++)
+            {
+                if (i > j)
+                    cout << "#";
+                else
+                    cout << " ";
+            }
+            cout << "\n";
+        }
+    break;
+    case 5:
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (i + j <= 10 - 1)
+                    cout << "#";
+                else
+                    cout << " ";
+            }
+            cout << "\n";
+        }
+    break;
+    case 6:
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (i + j >= 10 - 1)
+                    cout << "#";
+                else
+                    cout << " ";
+            }
+            cout << "\n";
+        }
+
+    }
